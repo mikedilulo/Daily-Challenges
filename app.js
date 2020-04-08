b49a8b295d50b789cc137e5c14858bc0c5b9724e
mikedilulo <mikedilulo2019@gmail.com>
2020-04-08 14:37:35 -0600

    Add function comments what I will work on 4-8-20

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //BELOW ARE MY EDABIT CHALLENGES. I ALSO HAVE A SEPARATE PROFILE CALLED iOS-mikedilulo, that is setup on my mac. There I am completing Swift Challenges
+
+// Addition of Two Numbers 4-8-2020
+
+// Subtraction of Two Numbers 4-8-2020
+
+// Division of Two Numbers 4-8-2020
+
+// Multiplication of Two Numbers 4-8-2020
